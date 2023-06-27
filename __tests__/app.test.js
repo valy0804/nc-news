62136041af0da0b8428537ce2002e3736634c76a
Valentina <loghin_valentina@yahoo.com>
2023-06-27 10:23:11 +0100

    implementing feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
 const db = require("../db/connection");
-//const connection = require("../db/connection");
 
 beforeEach(() => {
   return seed(testData);
@@ -33,7 +32,11 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then(({ body }) => {
-        expect(body.topics).toMatchObject(expectedTopics);
+        const { topics } = body;
+        expect(topics).toHaveLength(expectedTopics.length);
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("description", expect.any(String));
+        });
       });
   });
   test("404: responds with a not found message for an invalid endpoint", () => {
--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const express = require("express");
 
 const { getAllTopics } = require("./controllers/topic.controllers.js");
-const {
-  handleCustomErrors,
-  handlePsqlErrors,
-  handleServerErrors,
-} = require("./errors.js");
+const { handleCustomErrors, handleServerErrors } = require("./errors.js");
 
 const app = express();
-
-app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
 
 app.get("/api");
 
 app.use(handleCustomErrors);
-
-app.use(handlePsqlErrors);
 
 app.use(handleServerErrors);
 
