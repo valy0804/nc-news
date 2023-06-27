36f62d22cbd6cf3a42eb79ec3f6ba58e050db9de
Valentina <loghin_valentina@yahoo.com>
2023-06-27 16:32:40 +0100

    add get for get an article by id

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const express = require("express");
 
 const { getAllTopics } = require("./controllers/topic.controllers.js");
 const { getApi } = require("./controllers/api.controllers.js");
-const { handleCustomErrors, handleServerErrors } = require("./errors.js");
+const {
+  handleCustomErrors,
+  handleServerErrors,
+  handlePsqlErrors,
+} = require("./errors.js");
+const { getArticleById } = require("./controllers/articles.controllers.js");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
 
 app.get("/api", getApi);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.use(handleCustomErrors);
+
+app.use(handlePsqlErrors);
 
 app.use(handleServerErrors);
 
