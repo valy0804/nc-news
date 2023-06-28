d12cf622c1f6ab0b8f204c67fc52826b11af26f9
Valentina <loghin_valentina@yahoo.com>
2023-06-28 11:50:51 +0100

    getAllArticles controller fn

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,10 +1,21 @@
-const { selectArticleById } = require("../models/articles.models");
+const {
+  selectArticleById,
+  selectAllArticles,
+} = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getAllArticles = (req, res, next) => {
+  selectAllArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
     })
     .catch(next);
 };
