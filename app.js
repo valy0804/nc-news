0ba952f32908486637e9cf627e88fedaba0ac2a0
Valentina <loghin_valentina@yahoo.com>
2023-06-29 13:56:36 +0100

    add /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ const {
 } = require("./controllers/articles.controllers.js");
 const {
   getAllCommentsByArticleId,
+  addComment,
 } = require("./controllers/comments.controllers.js");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
 
@@ -26,6 +29,8 @@ app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getAllArticles);
 
 app.get("/api/articles/:article_id/comments", getAllCommentsByArticleId);
+
+app.post("/api/articles/:article_id/comments", addComment);
 
 app.use(handleCustomErrors);
 
