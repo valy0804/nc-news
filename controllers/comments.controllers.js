9ee4227cbe80ea5ed0309b8a0dde2889fa9a6c3a
Valentina <loghin_valentina@yahoo.com>
2023-06-29 13:57:54 +0100

    add addComment fn

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,4 +1,10 @@
-const { selectAllCommentsByArticleId } = require("../models/comments.models");
+const {
+  selectAllCommentsByArticleId,
+  insertComment,
+} = require("../models/comments.models");
+
+const { checkIfUsernameExists } = require("../models/users.models");
+const { checkArticleIdExists } = require("../models/articles.models");
 
 exports.getAllCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
@@ -6,6 +12,26 @@ exports.getAllCommentsByArticleId = (req, res, next) => {
   selectAllCommentsByArticleId(article_id)
     .then((comments) => {
       res.send({ comments });
+    })
+    .catch(next);
+};
+
+exports.addComment = (req, res, next) => {
+  const { article_id } = req.params;
+  const { username, body } = req.body;
+  const addCommentPromises = [];
+
+  if (article_id) {
+    addCommentPromises.push(checkArticleIdExists(article_id));
+  }
+  if (username) {
+    addCommentPromises.push(checkIfUsernameExists(username));
+  }
+
+  Promise.all(addCommentPromises)
+    .then(() => insertComment(article_id, username, body))
+    .then((comment) => {
+      res.status(201).send({ comment });
     })
     .catch(next);
 };
