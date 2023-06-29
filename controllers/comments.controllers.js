13fd5240c4fe2a80774f4c0a0896ecf63fed7d1c
Valentina <loghin_valentina@yahoo.com>
2023-06-29 11:32:26 +0100

    update selectAllCommentsByArticled fn

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,12 +2,9 @@ const { selectAllCommentsByArticleId } = require("../models/comments.models");
 
 exports.getAllCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
-  const { sort_by, order } = req.query;
-  const queries = [selectAllCommentsByArticleId(article_id, sort_by, order)];
-  if (article_id) queries.push(selectAllCommentsByArticleId(article_id));
 
-  Promise.all(queries)
-    .then(([comments]) => {
+  selectAllCommentsByArticleId(article_id)
+    .then((comments) => {
       res.send({ comments });
     })
     .catch(next);
