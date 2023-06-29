a1fba85e92b24f33a2f1f5a8ed47c1c87b227689
Valentina <loghin_valentina@yahoo.com>
2023-06-29 11:32:54 +0100

    update getAllCommentsByArticleId fn

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,18 +1,16 @@
 const db = require("../db/connection");
 
-exports.selectAllCommentsByArticleId = (
-  article_id,
-  sort_by = "created_at",
-  order = "desc"
-) => {
-  return db
-    .query(
-      `SELECT * FROM comments WHERE article_id = $1 ORDER BY ${sort_by} ${order}`,
-      [article_id]
-    )
+const { checkArticleIdExists } = require("./articles.models");
+
+exports.selectAllCommentsByArticleId = (article_id) => {
+  return checkArticleIdExists(article_id)
+    .then(() => {
+      return db.query(
+        `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`,
+        [article_id]
+      );
+    })
     .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Article not found" });
-      } else return rows;
+      return rows;
     });
 };
