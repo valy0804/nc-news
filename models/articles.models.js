75a4f1ec5bbe9fc2862b8af508a8ba7da4a9bbce
Valentina <loghin_valentina@yahoo.com>
2023-06-29 11:31:40 +0100

    add checkArticleIdExists fn

--- models/articles.models.js
+++ models/articles.models.js
@@ -32,3 +32,17 @@ exports.selectAllArticles = () => {
     return rows;
   });
 };
+
+exports.checkArticleIdExists = (article_id) => {
+  if (/\d+/.test(article_id)) {
+    return db
+      .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+      .then(({ rows }) => {
+        if (rows.length === 0) {
+          return Promise.reject({ status: 404, msg: "Article not found" });
+        } else return rows;
+      });
+  } else {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+};
