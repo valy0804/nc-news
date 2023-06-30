aab8921ec44c0eaa2d4f072b06c72e4f42af68f2
Valentina <loghin_valentina@yahoo.com>
2023-06-30 08:10:19 +0100

    add updateArticleVotesById fn

--- models/articles.models.js
+++ models/articles.models.js
@@ -46,3 +46,20 @@ exports.checkArticleIdExists = (article_id) => {
     return Promise.reject({ status: 400, msg: "Bad request" });
   }
 };
+
+exports.updateArticleVotesById = (article_id, inc_votes) => {
+  return db
+    .query(
+      `UPDATE articles
+      SET votes = votes + $1
+      WHERE article_id = $2
+      RETURNING *`,
+      [inc_votes, article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      return rows[0];
+    });
+};
