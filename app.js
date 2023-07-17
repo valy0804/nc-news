7169dbe5209b3f66665ea80bdbb9b8001082c0ca
Valentina <loghin_valentina@yahoo.com>
2023-07-17 17:18:57 +0300

    update cors

--- app.js
+++ app.js
@@ -21,10 +21,8 @@ const {
 const { getAllUsers } = require("./controllers/users.controllers.js");
 
 const app = express();
-
-app.use(express.json());
-
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
 
